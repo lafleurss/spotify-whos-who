5c2d503029ee3ebd29a64ab063b16ccf625bffd2
Shilpa Nair <101418016+lafleurss@users.noreply.github.com>
2023-04-18 14:19:06 -0500

    added Game functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { Howl, Howler } from "howler";
+import React, { useEffect, useState } from "react"
+import { Link, useHistory } from "react-router-dom"
+import styled from "styled-components"
+import { Howl, Howler } from "howler"
 
 const StyledGame = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-`;
+`
 
 const PlayButton = styled.button`
   width: 50px;
   height: 50px;
   border-radius: 50%;
   cursor: pointer;
-`;
+`
 
 const Song = styled.div`
   display: flex;
@@ -22,94 +23,153 @@ const Song = styled.div`
   width: 40%;
   border: 2px solid black;
   margin: 20px;
-`;
+`
 
 const ArtistContainer = styled.div`
   width: 80%;
   display: flex;
   justify-content: space-around;
   margin: 40px;
-`;
+`
 
 const Artist = styled.div`
-  height: 180px;
+  height: 190px;
   width: 150px;
   display: flex;
   flex-direction: column;
   border: 2px solid black;
   cursor: pointer;
-`;
+`
+
+const FinalScore = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 40%;
+  border: 2px solid black;
+  margin: 20px;
+`
 
 const Game = (props) => {
-  const [gameRound, setGameRound] = useState(0);
+  const history = useHistory()
+  const [gameRound, setGameRound] = useState(0)
+  const [disableNextButton, setDisableNextButton] = useState(false)
+  const [isCorrect, setIsCorrect] = useState(false)
+  const [isIncorrect, setIsIncorrect] = useState(false)
+  const [score, setScore] = useState(0)
+  const [showFinalScore, setShowFinalScore] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [currentSong, setCurrentSong] = useState(null)
 
-  console.log(props);
-  const guessData = props.location.state.guessDataComplete;
+  console.log(props)
+  const guessData = props.location.state.guessDataComplete
 
-  console.log(guessData);
-  console.log(guessData[gameRound].preview_url);
+  // toggles the song play or pause and the IsPlaying state
+  const toggleSong = (currentSong) => {
+    if (currentSong.playing()) {
+      currentSong.pause()
+      setIsPlaying(false)
+    } else {
+      currentSong.play()
+      setIsPlaying(true)
+    }
+  }
 
-  // const localSound = new Howl({ src: ["/sample.mp3"] });
+  const playOrPauseSong = () => {
+    //if song exists
+    if (currentSong) {
+      toggleSong(currentSong)
+    } else {
+      //if song not exists
+      const newSong = new Howl({
+        src: [guessData[gameRound].preview_url],
+        html5: true,
+        autoplay: true,
+      })
+      setCurrentSong(newSong)
+      toggleSong(newSong)
+    }
+  }
 
-  //plays preview of the song in the guessData array at the gameRound index
-  const songPreview = new Howl({
-    src: [guessData[gameRound].preview_url],
-    html5: true,
-    autoplay: true,
-  });
+  const handleNextRound = () => {
+    //Stop playing the song if next button is clicked
+    if (currentSong) {
+      currentSong.stop()
+    }
+    setCurrentSong(null)
+    setIsPlaying(false)
 
-  // toggles the song preview on or off
-  const playOrPauseSong = (howl) => {
-    howl.playing() ? howl.pause() : howl.play();
-    // add functionality to change button icon from play to pause
-  };
+    //Checking to see when the next button is to be disabled
+    if (gameRound === guessData.length - 1) {
+      setDisableNextButton(true)
+      setShowFinalScore(true)
+    } else {
+      //else increment gameRound to get next Song and Choices
+      setGameRound(gameRound + 1)
+    }
+  }
 
-  // runs when user clicks on an artist.
+  const startNewGame = () => {
+    history.push("/")
+  }
+
   const submitArtistChoice = (choice) => {
-    console.log("artist choice: ", choice);
+    console.log("artist choice: ", choice)
     // TODO: add styling to distinguish which artist was chosen
 
     //check if the choice is correct
-    // TODO: convert to ternary
-    if (choice === guessData[gameRound].Artist) {
-      // TODO: show 'Correct' on screen
+    if (choice.name === guessData[gameRound].artist) {
+      // alert("Correct!")
+      setIsCorrect(true)
+      setIsIncorrect(false)
+      setScore(score + 1)
+      handleNextRound()
     } else {
-      // TODO: show 'Sorry, that's incorrect' on screen
+      // alert("Incorrect!")
+      setIsCorrect(false)
+      setIsIncorrect(true)
+      handleNextRound()
     }
-    // TODO: save to list or adjust score if we have multiple rounds
-
-    // TODO: go to next round or show results screen
-  };
+  }
 
   return (
     <StyledGame>
-      <Song>
-        <h4>Song:</h4>
-        <h2>{guessData[gameRound].name}</h2>
-        <PlayButton onClick={() => playOrPauseSong(songPreview)}>
-          Play
-        </PlayButton>
-      </Song>
-      <ArtistContainer>
-        {guessData[gameRound].choices.map((choice) => (
-          <Artist onClick={() => submitArtistChoice(choice)}>
-            <img src={choice.imgUrl} />
-            <span>{choice.name}</span>
-          </Artist>
-        ))}
-      </ArtistContainer>
-
-      <h1 style={{marginTop: '200px'}}>Guess Data:</h1>
-      <ul>
-        {Object.keys(guessData).map((key) => (
-          <li key={key}>
-            <strong>{key}: </strong>
-            {JSON.stringify(guessData[key])}
-          </li>
-        ))}
-      </ul>
+      {!showFinalScore && (
+        <>
+          <Song>
+            <h4>Song:</h4>
+            <h2>{guessData[gameRound].name}</h2>
+            <PlayButton onClick={() => playOrPauseSong()}>
+              {isPlaying ? "Pause" : "Play"}
+            </PlayButton>
+          </Song>
+          <ArtistContainer>
+            {guessData[gameRound].choices.map((choice, index) => (
+              <Artist key={index} onClick={() => submitArtistChoice(choice)}>
+                <img src={choice.imgUrl} />
+                <span>{choice.name}</span>
+              </Artist>
+            ))}
+          </ArtistContainer>
+          <button disabled={disableNextButton} onClick={handleNextRound}>
+            Next Song
+          </button>
+          {isCorrect === true && <p>Correct!</p>}
+          {isCorrect === false && <p>Incorrect!</p>}
+          <div>Current Score: {score}</div>
+        </>
+      )}
+      {showFinalScore && (
+        <FinalScore>
+          {" "}
+          <h2>
+            Final Score: {score} out of {guessData.length}{" "}
+          </h2>
+          <button onClick={startNewGame}>New Game</button>
+        </FinalScore>
+      )}
     </StyledGame>
-  );
-};
+  )
+}
 
-export default Game;
+export default Game
