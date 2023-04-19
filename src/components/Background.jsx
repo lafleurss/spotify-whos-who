479f4044633aa1968e773e59d55cab9b766f302d
Shilpa Nair <101418016+lafleurss@users.noreply.github.com>
2023-04-19 15:07:22 -0500

    added

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,7 +1,10 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 const Background = styled.div`
   background: url("cheers.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-color: #000000;
   height: 100vh;
   width: 100vw;
   display: flex;
